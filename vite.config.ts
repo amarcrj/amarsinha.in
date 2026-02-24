148f5bc267672328d45bed211a0614af524bf732
Amar Sinha <amaraec1720@gmail.com>
2026-02-24 20:09:34 +0530

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+export default defineConfig({
+  base: "/",   // 🔥 MUST match repo name exactly
+  plugins: [react(), componentTagger()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
